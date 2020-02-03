f999ee3fcf42e28cae185aee0a887bb917c6c0c3
gabrielespinosa936 <gabriel.espinosa936@myci.csuci.edu>
2020-02-02 22:43:33 -0800

    Added UIButton and UITextfield programatically

---
+++ GHFollowrtd/GHFollowrtd/GFButton.swift
@@ -0,0 +1,41 @@
+//
+//  GFButton.swift
+//  GHFollowrtd
+//
+//  Created by Gabriel Espinosa on 2/2/20.
+//  Copyright © 2020 Gabriel Espinosa . All rights reserved.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        // Now build on default button
+        configure()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor : UIColor , title : String)
+    {
+        super.init(frame : .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure()
+    {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        // This tells xcode to use autolayout
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
