7e38ae659f6eff657a9ca5fc8f853f38487b43c8
gabrielespinosa938 <Gabriel.espinosa936@myci.csuci.edu>
2020-02-05 08:39:14 -0800

    Search navigation needs work

---
+++ GHFollowrtd/GHFollowrtd/FollowerListVC.swift
@@ -0,0 +1,23 @@
+//
+//  FollowerListVC.swift
+//  GHFollowrtd
+//
+//  Created by Gabriel Espinosa  on 2/5/20.
+//  Copyright © 2020 Gabriel Espinosa . All rights reserved.
+//
+
+import UIKit
+
+class FollowerListVC: UIViewController {
+    
+    var userName : String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        navigationController?.isNavigationBarHidden = false
+    }
+
+}
--- GHFollowrtd/GHFollowrtd/SearchVC.swift
+++ GHFollowrtd/GHFollowrtd/SearchVC.swift
@@ -37,6 +37,14 @@ class SearchVC: UIViewController {
         view.addGestureRecognizer(tap)
     }
     
+    @objc func pushFollowerListVC()
+    {
+        let followeListVC = FollowerListVC()
+        followeListVC.userName = usernameTextField.text
+        followeListVC.title = usernameTextField.text
+        navigationController?.pushViewController(followeListVC, animated: true)
+    }
+    
     func configureLogoImageview()
     {
         view.addSubview(logoImageView)
@@ -65,7 +73,8 @@ class SearchVC: UIViewController {
     func configureCallToActionButton()
     {
         view.addSubview(callToActionButton)
-
+        callToActionButton.addTarget(self, action: #selector(pushFollowerListVC), for: .touchUpInside)
+        
         NSLayoutConstraint.activate([
             callToActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
             callToActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
@@ -79,6 +88,8 @@ class SearchVC: UIViewController {
 extension SearchVC : UITextFieldDelegate
 {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        pushFollowerListVC()
         print("Did tap return")
         return true
     }
