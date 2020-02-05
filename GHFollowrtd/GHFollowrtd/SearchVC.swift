eac79e7dbf887ad26cfe658c8bbae0f3f36d0694
gabrielespinosa938 <Gabriel.espinosa936@myci.csuci.edu>
2020-02-04 22:13:13 -0800

    Fixed the UIButton

--- GHFollowrtd/GHFollowrtd/SearchVC.swift
+++ GHFollowrtd/GHFollowrtd/SearchVC.swift
@@ -20,7 +20,10 @@ class SearchVC: UIViewController {
 
         // Do any additional setup after loading the view.
         
-        view.backgroundColor = .systemBackground
+        configureLogoImageview()
+        configureTextField()
+        configureCallToActionButton()
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -28,17 +31,39 @@ class SearchVC: UIViewController {
         navigationController?.isNavigationBarHidden = true
     }
     
-    func configure()
+    func configureLogoImageview()
     {
         view.addSubview(logoImageView)
         logoImageView.translatesAutoresizingMaskIntoConstraints = false
         logoImageView.image = UIImage(named: "gh-logo")!
-        
+
         NSLayoutConstraint.activate([
             logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
             logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             logoImageView.heightAnchor.constraint(equalToConstant: 200),
             logoImageView.widthAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+    func configureTextField()
+    {
+        view.addSubview(usernameTextField)
+        NSLayoutConstraint.activate([
+            usernameTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 48),
+            usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            usernameTextField.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    func configureCallToActionButton()
+    {
+        view.addSubview(callToActionButton)
+
+        NSLayoutConstraint.activate([
+            callToActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            callToActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            callToActionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            callToActionButton.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
 
